a4010cc6cf3a7dc828843fe2d7d59ff72e2e67c1
eliza29 <elizaseguraf@gmail.com>
2018-03-29 22:34:47 -0500

    añadiendo avance de home

--- js/mapa.js
+++ js/mapa.js
@@ -9,6 +9,7 @@ var platform = new H.service.Platform({
 var maptypes = platform.createDefaultLayers();
 
 // Instantiate (and display) a map object:
+
 var map = new H.Map(
 document.getElementById('mapContainer'),
 maptypes.normal.map,
@@ -16,3 +17,7 @@ maptypes.normal.map,
   zoom: 10,
   center: { lng: 13.4, lat: 52.51 }
 });
+var icon = new H.map.Icon('assets/images/mimarcador.png');
+var marker = new H.map.Marker({ lat: 52.5, lng: 13.4 }, { icon: icon });
+// Add the marker to the map:
+map.addObject(marker);
