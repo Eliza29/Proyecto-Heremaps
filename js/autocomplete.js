7cf8c350eee4165aa9e813557d78d82bf48809fb
eliza29 <elizaseguraf@gmail.com>
2018-04-02 22:19:43 -0500

    añadiendo info para modificar

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,63 +1,63 @@
-var AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json',
-    ajaxRequest = new XMLHttpRequest(),
-    query = '';
+// var AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json',
+//     ajaxRequest = new XMLHttpRequest(),
+//     query = '';
 
-/**
- * If the text in the text box  has changed, and is not empty,
- * send a geocoding auto-completion request to the server.
- *
- * @param {Object} textBox the textBox DOM object linked to this event
- * @param {Object} event the DOM event which fired this listener
- */
-function autoCompleteListener(textBox, event) {
+// /**
+//  * If the text in the text box  has changed, and is not empty,
+//  * send a geocoding auto-completion request to the server.
+//  *
+//  * @param {Object} textBox the textBox DOM object linked to this event
+//  * @param {Object} event the DOM event which fired this listener
+//  */
+// function autoCompleteListener(textBox, event) {
 
-  if (query != textBox.value){
-    if (textBox.value.length >= 1){
+//   if (query != textBox.value){
+//     if (textBox.value.length >= 1){
 
-      /**
-      * A full list of available request parameters can be found in the Geocoder Autocompletion
-      * API documentation.
-      *
-      */
-      var params = '?' +
-        'query=' +  encodeURIComponent(textBox.value) +   // The search text which is the basis of the query
-        '&beginHighlight=' + encodeURIComponent('<mark>') + //  Mark the beginning of the match in a token. 
-        '&endHighlight=' + encodeURIComponent('</mark>') + //  Mark the end of the match in a token. 
-        '&maxresults=5' +  // The upper limit the for number of suggestions to be included 
-                          // in the response.  Default is set to 5.
-        '&app_id=' + APPLICATION_ID +
-        '&app_code=' + APPLICATION_CODE;
-      ajaxRequest.open('GET', AUTOCOMPLETION_URL + params );
-      ajaxRequest.send();
-    }
-  }
-  query = textBox.value;
-}
-
-
-/**
- *  This is the event listener which processes the XMLHttpRequest response returned from the server.
- */
-function onAutoCompleteSuccess() {
- /*
-  * The styling of the suggestions response on the map is entirely under the developer's control.
-  * A representitive styling can be found the full JS + HTML code of this example
-  * in the functions below:
-  */
-  clearOldSuggestions();
-  addSuggestionsToPanel(this.response);  // In this context, 'this' means the XMLHttpRequest itself.
-  addSuggestionsToMap(this.response);
-}
+//       /**
+//       * A full list of available request parameters can be found in the Geocoder Autocompletion
+//       * API documentation.
+//       *
+//       */
+//       var params = '?' +
+//         'query=' +  encodeURIComponent(textBox.value) +   // The search text which is the basis of the query
+//         '&beginHighlight=' + encodeURIComponent('<mark>') + //  Mark the beginning of the match in a token. 
+//         '&endHighlight=' + encodeURIComponent('</mark>') + //  Mark the end of the match in a token. 
+//         '&maxresults=5' +  // The upper limit the for number of suggestions to be included 
+//                           // in the response.  Default is set to 5.
+//         '&app_id=' + APPLICATION_ID +
+//         '&app_code=' + APPLICATION_CODE;
+//       ajaxRequest.open('GET', AUTOCOMPLETION_URL + params );
+//       ajaxRequest.send();
+//     }
+//   }
+//   query = textBox.value;
+// }
 
 
-/**
- * This function will be called if a communication error occurs during the XMLHttpRequest
- */
-function onAutoCompleteFailed() {
-  alert('Ooops!');
-}
+// /**
+//  *  This is the event listener which processes the XMLHttpRequest response returned from the server.
+//  */
+// function onAutoCompleteSuccess() {
+//  /*
+//   * The styling of the suggestions response on the map is entirely under the developer's control.
+//   * A representitive styling can be found the full JS + HTML code of this example
+//   * in the functions below:
+//   */
+//   clearOldSuggestions();
+//   addSuggestionsToPanel(this.response);  // In this context, 'this' means the XMLHttpRequest itself.
+//   addSuggestionsToMap(this.response);
+// }
 
-// Attach the event listeners to the XMLHttpRequest object
-ajaxRequest.addEventListener("load", onAutoCompleteSuccess);
-ajaxRequest.addEventListener("error", onAutoCompleteFailed);
-ajaxRequest.responseType = "json";
+
+// /**
+//  * This function will be called if a communication error occurs during the XMLHttpRequest
+//  */
+// function onAutoCompleteFailed() {
+//   alert('Ooops!');
+// }
+
+// // Attach the event listeners to the XMLHttpRequest object
+// ajaxRequest.addEventListener("load", onAutoCompleteSuccess);
+// ajaxRequest.addEventListener("error", onAutoCompleteFailed);
+// ajaxRequest.responseType = "json";
--- js/home.js
+++ js/home.js
@@ -1,10 +1,12 @@
+var APPLICATION_ID = 'esgpefgkDV0uRo2GWinI',
+APPLICATION_CODE = 'Qcq4vKeZx9k1cI-8kJyglw';
 
 // mapa 
-// Initialize the platform object:
 
+// Initialize the platform object:
 var platform = new H.service.Platform({
-  app_id: 'esgpefgkDV0uRo2GWinI',
-  app_code: 'Qcq4vKeZx9k1cI-8kJyglw',
+  app_id: APPLICATION_ID,
+  app_code: APPLICATION_CODE,
   useCIT: true,
   useHTTPS: true
 });
@@ -34,15 +36,108 @@ if (navigator.geolocation) {
     map.setZoom(12);
     var icon = new H.map.Icon('assets/images/mimarcador.png');
     var marker = new H.map.Marker(pos, { icon: icon });
-
     // Add the marker to the map:
     map.addObject(marker);
+
+    function explorePlaces(platform) {
+      var explore = new H.places.Explore(platform.getPlacesService());
+      var params = {
+        'cat': 'natural-geographical, leisure-outdoor',
+        'in': '-3.73165,-73.24078;r=10000'  // Peru
+      };
+      
+      explore.request(params, {}, onResult, onError);
+    }
+    // para mostrar mapas y tarjetas ver aqui
+    // explorePlaces(platform);
   });
 } else {
   alert("Posición no disponible");
 }
+
+// places
+
+
+function onResult(result) {
+  var places = result.results.items;
+  console.log(places);
+  addPlacesToMap(places);
+  addPlacesToPanel(places);
+}
+
+function onError(error) {
+  error = data;
+}
+
+var bubble;
+
+function openBubble(position, text){
+  if(!bubble){
+    bubble =  new H.ui.InfoBubble(
+      position,
+      // The FO property holds the province name.
+      {content: text});
+    ui.addBubble(bubble);
+  } else {
+    bubble.setPosition(position);
+    bubble.setContent(text);
+    bubble.open();
+  }
+}
+function addPlacesToMap(places) {
+  var group = new  H.map.Group();
+  // add 'tap' event listener, that opens info bubble, to the group
+  group.addEventListener('tap', function (evt) {
+    map.setCenter(evt.target.getPosition());
+    openBubble(
+      evt.target.getPosition(), evt.target.content);
+  }, false);
+
+  group.addObjects(places.map(function (place) {
+    var marker = new H.map.Marker({lat: place.position[0], lng: place.position[1]})
+    marker.content = '<div style="font-size: 10px" ><h3>' + place.title +
+      '</h3><h4>' + place.category.title + '</h4>' + place.vicinity + '</div>';
+    return marker;
+  }));
+
+  map.addObject(group);
+
+  // get geo bounding box for the group and set it to the map
+  map.setViewBounds(group.getBounds());
+}
+function addPlacesToPanel(places){
+
+  var nodeOL = document.createElement('ul'),
+    i;
+
+  nodeOL.style.fontSize = 'small';
+  nodeOL.style.marginLeft ='5%';
+  nodeOL.style.marginRight ='5%';
+
+   for (i = 0;  i < places.length; i += 1) {
+     var li = document.createElement('li'),
+        divLabel = document.createElement('div'),
+        content =  '<strong style="font-size: large;">' + places[i].title  + '</strong>';
+        content += '&nbsp;<span style="font-size:smaller">(' +  places[i].category.title + ')</span></br>';
+        content +=  places[i].vicinity + '</br>';
+        content += '<strong>distance:</strong>' +  places[i].distance + 'm</br>';
+
+      divLabel.innerHTML = content;
+      li.appendChild(divLabel);
+      nodeOL.appendChild(li);
+  }
+
+  placesContainer.appendChild(nodeOL);
+}
+
+
+
+var placesContainer = document.getElementById('panel');
 //Step 3: make the map interactive
 var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
 // Create the default UI components
 var ui = H.ui.UI.createDefault(map, maptypes);
+
+// places
+
